4064222c5cea7cabfb0f65bda67348ad45fe440b
Benjamin Guma <benjaminguma.bg@gmail.com>
2025-04-24 13:32:54 +0100

    _

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyDmBnk4XnzSEq0wPXBW1tHAHgOcUTICGsg",
-	authDomain: "awesome-project-5cb2a.firebaseapp.com",
-	projectId: "awesome-project-5cb2a",
-	storageBucket: "awesome-project-5cb2a.firebasestorage.app",
-	messagingSenderId: "847822676051",
-	appId: "1:847822676051:web:3c0905a04a5984c4e0a6a6",
-	measurementId: "G-5R1RFZ5R2R",
+	apiKey: "AIzaSyA9Fwlzn9R6Z47zfh2pKdjwlYEjuV--yrE",
+	authDomain: "hopterlinkapp.firebaseapp.com",
+	projectId: "hopterlinkapp",
+	storageBucket: "hopterlinkapp.firebasestorage.app",
+	messagingSenderId: "217728306884",
+	appId: "1:217728306884:web:267e79b4d99ed383a78391",
+	measurementId: "G-2RT7PBG5G3",
 };
 
 // Initialize Firebase
